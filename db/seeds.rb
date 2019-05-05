854244eb138dc388a7a7fb34d4d18f9ffc2f7362
Christia Tidwell <christiatidwell@Christias-MBP.attlocal.net>
2019-05-05 17:00:59 -0500

    a seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+
+# flight.create(name: "P101" , destination: "Paris")
+# flight.create(name: "N101" , destination: "New York")
+# flight.create(name: "C101" , destination: "Chicago")
+# flight.create(name: "D101" , destination: "Dallas")
+# flight.create(name: "W101", destination: "Washington, DC")
