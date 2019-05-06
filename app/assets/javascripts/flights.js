214995ab0ea12e18c0ed512c7481c4fb94383101
Christia Tidwell <christiatidwell@Christias-MBP.attlocal.net>
2019-05-06 16:45:06 -0500

    got index of flights working

--- app/assets/javascripts/flights.js
+++ app/assets/javascripts/flights.js
@@ -15,40 +15,47 @@ function getFlights() {
     url: 'http://localhost:3000/flights',
     method: 'get',
     dataType: 'json'
-  }).done (function(data) {
-    console.log("the data is", data)
+  }).done (function(flights) {
+    //console.log("the data is", data)
 //debugger
-    let myflight = new Flight(data[0])
-    debugger
-    let myFlighthtml = myflight.flighthtml()
+  flights.forEach(flight => {
+    let myflight = new Flight(flight)
+      //console.log(myflight)
+      let myFlighthtml = myflight.flighthtml()
+      document.getElementById('ajax-flights').innerHTML += myFlighthtml
+      console.log(myFlighthtml)
+  })
+//     let myflight = new Flight(data[0])
+//       .map(element => {
+//       return (`
+//         <p>${element.name}</p>
+//         `)
+//   })
+// )
+//    debugger
+
+    //debugger
   })
 }
 
-class Flight {
-  constructor(obj) {
-    this.id = obj.id
-    this.name = obj.name
-    this.destination = obj.destination
-    this.bookings = obj.bookings
-    this.users = obj.users
-  }
+function Flight(flight) {
+    this.id = flight.id
+    this.name = flight.name
+    this.destination = flight.destination
+    this.bookings = flight.bookings
+    this.users = flight.users
 }
 
 Flight.prototype.flighthtml = function() {
-  let flightBookings = this.bookings.map(booking) => {
-    return
-      {`
-        <p>booking.flight_id<br>
-        booking.description<br>
-        booking.paid</p>\
-      `}
-  }
-
-  return {`
-    <div>
-    <h3>${this.destination}</h3>
-    <p>${this.name}</p>
-    <p>${flightBookings}</p>
-    </div>
-    `}
+  // let flightBookings = this.bookings.map(booking => {
+  //   return (`
+  //     <p>${booking.description}<br></p>
+  //     `)
+  //   })
+    return (`
+      <div>
+      <h3>${this.destination}</h3>
+      <p>${this.name}</p>
+      </div>
+      `)
 }
