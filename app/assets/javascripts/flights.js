4927b3e76243e81e29f25c89290bc6bbb8ff136f
Christia Tidwell <christiatidwell@Christias-MBP.attlocal.net>
2019-05-05 20:39:00 -0500

    added flight constructor and prototype

--- app/assets/javascripts/flights.js
+++ app/assets/javascripts/flights.js
@@ -17,8 +17,38 @@ function getFlights() {
     dataType: 'json'
   }).done (function(data) {
     console.log("the data is", data)
-debugger
+//debugger
     let myflight = new Flight(data[0])
-    let myFlighthtml = myflight.flight.html()
+    debugger
+    let myFlighthtml = myflight.flighthtml()
   })
+}
+
+class Flight {
+  constructor(obj) {
+    this.id = obj.id
+    this.name = obj.name
+    this.destination = obj.destination
+    this.bookings = obj.bookings
+    this.users = obj.users
+  }
+}
+
+Flight.prototype.flighthtml = function() {
+  let flightBookings = this.bookings.map(booking) => {
+    return
+      {`
+        <p>booking.flight_id<br>
+        booking.description<br>
+        booking.paid</p>\
+      `}
+  }
+
+  return {`
+    <div>
+    <h3>${this.destination}</h3>
+    <p>${this.name}</p>
+    <p>${flightBookings}</p>
+    </div>
+    `}
 }
