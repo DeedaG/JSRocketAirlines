5013be6ad526f2517f8f77a5cc689c47627895db
Christia Tidwell <christiatidwell@Christias-MBP.attlocal.net>
2019-05-05 19:41:08 -0500

    added getFlights function

--- app/assets/javascripts/flights.js
+++ app/assets/javascripts/flights.js
@@ -4,8 +4,21 @@ $(function() {
 });
 
 function listenForClick() {
-  $('button').on('click', function(event) {
-    event.preventDefault()
-    getFlights()
+ $("button#flights-data").on('click', function(event) {
+   event.preventDefault()
+   getFlights()
+  })
+}
+
+function getFlights() {
+  $.ajax({
+    url: 'http://localhost:3000/flights',
+    method: 'get',
+    dataType: 'json'
+  }).done (function(data) {
+    console.log("the data is", data)
+debugger
+    let myflight = new Flight(data[0])
+    let myFlighthtml = myflight.flight.html()
   })
 }
