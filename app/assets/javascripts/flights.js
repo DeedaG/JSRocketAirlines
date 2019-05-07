02c59ff1d9ca86460db3c37fd68845c86ff86308
Christia Tidwell <christiatidwell@Christias-MBP.attlocal.net>
2019-05-06 20:03:06 -0500

    working on new booking form

--- app/assets/javascripts/flights.js
+++ app/assets/javascripts/flights.js
@@ -1,6 +1,7 @@
 $(function() {
   console.log("flights.js is loaded...")
   listenForClick()
+  listenForNewBookingFormClick()
 });
 
 function listenForClick() {
@@ -25,37 +26,52 @@ function getFlights() {
       document.getElementById('ajax-flights').innerHTML += myFlighthtml
       console.log(myFlighthtml)
   })
-//     let myflight = new Flight(data[0])
-//       .map(element => {
-//       return (`
-//         <p>${element.name}</p>
-//         `)
-//   })
-// )
-//    debugger
 
-    //debugger
   })
 }
 
-function Flight(flight) {
-    this.id = flight.id
-    this.name = flight.name
-    this.destination = flight.destination
-    this.bookings = flight.bookings
-    this.users = flight.users
+class Flight {
+  constructor(obj) {
+    this.id = obj.id
+    this.name = obj.name
+    this.destination = obj.destination
+    this.bookings = obj.bookings
+    this.users = obj.users
+  }
+    static newBookingForm(){
+      return (`
+
+        <form>
+          <input id='flight-name' type='text' name='name'></input><br>
+          <input type= 'text' name='destination'></input><br>
+          <input type='submit' />
+        </form>
+        `)
+    }
 }
 
+function listenForNewBookingFormClick() {
+ $("button#ajax-new-booking").on('click', function(event) {
+   event.preventDefault()
+   let newBookingForm = Flight.newBookingForm()
+   document.querySelector('div#new-booking-form-div').innerHTML = newBookingForm
+   //debugger
+  })
+}
+
+
+
 Flight.prototype.flighthtml = function() {
-  // let flightBookings = this.bookings.map(booking => {
-  //   return (`
-  //     <p>${booking.description}<br></p>
-  //     `)
-  //   })
+   // let flightBookings = this.bookings.map(booking => {
+   //   return (`
+   //        ${booking.paid}
+   //      `)
+   // })
+
     return (`
       <div>
-      <h3>${this.destination}</h3>
-      <p>${this.name}</p>
+      <h3>${this.destination}</h3><p>${this.name}</p>
+      <br>
       </div>
       `)
 }
