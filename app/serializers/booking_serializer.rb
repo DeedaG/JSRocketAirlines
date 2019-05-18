e215876fa3d4325f794a12673a6ce77a1996b136
Christia Tidwell <christiatidwell@Christias-MBP.attlocal.net>
2019-05-18 16:35:46 -0500

    added new attributes to bookings serializer

--- app/serializers/booking_serializer.rb
+++ app/serializers/booking_serializer.rb
@@ -1,5 +1,5 @@
 class BookingSerializer < ActiveModel::Serializer
-  attributes :id, :description, :flight_id, :user_id, :paid, :created_at
+  attributes :id, :description, :flight_id, :user_id, :paid, :created_at, :luggage, :flight_class
 
   belongs_to :flight
   belongs_to :user
